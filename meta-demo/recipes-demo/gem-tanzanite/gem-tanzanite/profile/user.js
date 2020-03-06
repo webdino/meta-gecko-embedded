42eee4bac1d7ed2f86b377cbfc042b25a1d3078a
dynamis <dynamis@firehacks.org>
2020-03-06 09:19:24 +0900

    allow file:// url loading for Gecko 68

--- meta-demo/recipes-demo/gem-tanzanite/gem-tanzanite/profile/user.js
+++ meta-demo/recipes-demo/gem-tanzanite/gem-tanzanite/profile/user.js
@@ -4,3 +4,5 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.shell.skipDefaultBrowserCheck", true);
 user_pref("browser.shell.skipDefaultBrowserCheckOnFirstRun", true);
 user_pref("dom.w3c_touch_events.enabled", 3);
+// allow fetch/woff loading from file:// url (Gecko 68 or later)
+user_pref("privacy.file_unique_origin", false);
